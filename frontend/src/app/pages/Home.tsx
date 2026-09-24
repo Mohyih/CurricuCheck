115749573e07a3fa22f17e92e18c1b5f6b91b266
Francisco M. Macapagal Jr <mohyihfm@yahoo.com>
2026-09-24 23:15:13 +0800

    Insight Revisions v14

--- frontend/src/app/pages/Home.tsx
+++ frontend/src/app/pages/Home.tsx
@@ -14,6 +14,7 @@ export function Home() {
     const handleOffline = () => setIsOnline(false);
     const savedScroll = sessionStorage.getItem("homeScroll");
 
+    
     if (savedScroll) {
   requestAnimationFrame(() => {
     window.scrollTo({
